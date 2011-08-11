43321d8d67bb4c8fd7ec1aaf170433fcdff9eb82
Mark Rickerby <me@maetl.net>
2011-08-11 13:07:23 +1000

    fixing mapping methods

--- BigCommerce/Api2.php
+++ BigCommerce/Api2.php
@@ -51,62 +51,72 @@ class BigCommerce_Api2
 		 	self::$connection = new BigCommerce_Api2_Connection();
 			self::$connection->authenticate(self::$username, self::$api_key);
 		}
+
 		return self::$connection;
 	}
 
 	private static function makeResource($resource, $object)
 	{
 		$class = 'BigCommerce_Api2_' . $resource;
+
 		return new $class($object);
 	}
 
-	private static function mapCollection($response, $resource)
+	private static function mapCollection($resource, $object)
 	{
-		if ($response == false || is_string($response)) return $response;
+		if ($object == false || is_string($object)) return $object;
 
 		self::$resource = $resource;
 
-		return array_map(array('self', 'mapCollectionObject'), $response);
+		return array_map(array('self', 'mapCollectionObject'), $object);
 	}
 
-	private static function mapCollectionObject($object) {
+	private static function mapCollectionObject($object)
+	{
 		return self::makeResource(self::$resource, $object);
 	}
 
-	private static function mapResource($response, $resource)
+	private static function mapResource($resource, $object)
 	{
-		if ($response == false || is_string($response)) return $response;
+		if ($object == false || is_string($object)) return $object;
 
-		return self::makeResource($resource, $response);
+		return self::makeResource($object, $resource);
 	}
 
-	private static function mapCount($resource) {
-		if ($response == false || is_string($response)) return $response;
+	private static function mapCount($object) {
+		if ($object == false || is_string($object)) return $object;
 
-		return $response->count;
+		return $object->count;
 	}
 
 	public static function ping()
 	{
-		return self::connection()->get(self::$api_path . '/time');
+		$response = self::connection()->get(self::$api_path . '/time');
+
+		if ($response == false || is_string($response)) return $response;
+
+		return new DateTime("@{$response->time}");
 	}
 
 	public static function getProducts()
 	{
-		$products = self::connection()->get(self::$api_path . '/products');
-		return self::mapCollection($products, 'Product');
+		$response = self::connection()->get(self::$api_path . '/products');
+
+		return self::mapCollection('Product', $response);
 	}
 
 	public static function getProductsCount()
 	{
-		$products = self::connection()->get(self::$api_path . '/products/count');
-		return self::mapCount($products);
+		$response = self::connection()->get(self::$api_path . '/products/count');
+
+		return self::mapCount($response);
 	}
 
 	public static function getProduct()
 	{
-		$product = self::connection()->get(self::$api_path . '/time');
-		return self::mapResource($product, 'Product');
+		$response = self::connection()->get(self::$api_path . '/time');
+
+		return self::mapResource('Product', $response);
 	}
 
 	public static function updateProduct()
