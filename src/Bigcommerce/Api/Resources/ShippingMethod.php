a0ea16f35b80aa17239d55938bf06923846d5740
Craig Morrissey <craig.morrissey@huglogix.com>
2016-08-17 21:18:13 +0000

    Add getShippingMethods

---
+++ src/Bigcommerce/Api/Resources/ShippingMethod.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bigcommerce\Api\Resources;
+
+use Bigcommerce\Api\Resource;
+use Bigcommerce\Api\Client;
+
+/**
+ * A shipping method.
+ */
+class ShippingMethod extends Resource
+{
+
+}
