e4927dcf6fe661452c258bdbcbec80255e1f8ac8
Ivan Shcherbak <alotofall@gmail.com>
2024-01-10 10:18:17 +0200

    Use rector for test files only

--- rector.php
+++ rector.php
@@ -10,10 +10,7 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/test',
-        __DIR__ . '/src',
     ]);
 
-
-    // define sets of rules
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_81, PHPUnitSetList::PHPUNIT_100,]);
+    $rectorConfig->sets([PHPUnitSetList::PHPUNIT_100,]);
 };
